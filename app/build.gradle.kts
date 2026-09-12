6e23d3fcb2fcb38aae08866fdae396aa3e5f7d6b
bab4bu-maker <bab4bu@gmail.com>
2026-09-12 21:43:35 +0700

    Bump app version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.akanclip.tradingalert"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     compileOptions {
